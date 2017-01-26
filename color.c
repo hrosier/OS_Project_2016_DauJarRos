aa15a1de4023c67e8c1bd02ccc9ea53a1d61adbc
Hubert ROSIER <h.rosier43@gmail.com>
2017-01-26 21:39:44 +0100

    Add comments and write some details about the functions used. It takes me a lot of time, I don't think I am going to do this for all the files ...

--- color.c
+++ color.c
@@ -5,6 +5,7 @@ const char const *color[] = { "?", "BLACK", "BLUE", "GREEN", "YELLOW", "RED", "W
 
 void print_RGB(uint8_t sn_color){
   float val0, val1, val2;
+  // we change the mode of the sensor to RGB_RAW to get the trichromic values
   set_sensor_mode(sn_color, "RGB_RAW");
   get_sensor_value0(sn_color,&val0);
   get_sensor_value0(sn_color,&val1);
--- headers/final.h
+++ headers/final.h
@@ -1,9 +1,85 @@
+/** 
+\brief : function used to make the robot run the small arena as the beginner
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor 
+* \param uint8_t sn_color : the sequence number of the color sensor
+* \param int* s : the adress where is stored the number corresponding to the BT connexion  
+* \param int dest : the ID of the ally 
+*/
 void small_arena_beginner(uint8_t *sn, uint8_t sn3, uint8_t sn_sonar,uint8_t sn_color, int s, int dest);
+
+/** \brief : function used to make the robot put the ball in the middle of the small arena as the beginner
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor 
+* \param int* s : the adress where is stored the number corresponding to the BT connexion (used to send messages)
+* \param int dest : the ID of the ally (used to send messages)
+*/
 void put_small_beg(uint8_t *sn, uint8_t sn3, uint8_t sn_sonar, int s, int dest);
+
+/**
+* \brief : function used to make the robot get the ball in the middle of the small arena as the beginner
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor
+* \param uint8_t sn_color : the sequence number of the color sensor
+* \param int* s : the adress where is stored the number corresponding to the BT connexion (used to send messages)
+* \param int dest : the ID of the ally (used to send messages)
+*/
 void get_small_beg(uint8_t *sn, uint8_t sn3, uint8_t sn_sonar, uint8_t sn_color, int s, int dest);
+
+/**
+* \brief : function used to make the robot run the small arena as the finisher
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor 
+* \param uint8_t sn_color : the sequence number of the color sensor
+* \param int* s : the adress where is stored the number corresponding to the BT connexion  
+* \param int dest : the ID of the ally 
+*/
 void small_arena_finisher(uint8_t *sn, uint8_t sn3, uint8_t sn_sonar, uint8_t sn_color, int s, int dest);
+
+/**
+* \brief : function used to make the robot get the ball in the middle of the small arena as the finisher
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor
+* \param uint8_t sn_color : the sequence number of the color sensor
+* \param int* s : the adress where is stored the number corresponding to the BT connexion (used to send messages)
+* \param int dest : the ID of the ally (used to send messages)
+*/
 void get_small_fin(uint8_t *sn, uint8_t sn3, uint8_t sn_sonar, uint8_t sn_color, int s, int dest);
+
+/** \brief : function used to make the robot put the ball in the middle of the small arena as the beginner
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor 
+* \param int* s : the adress where is stored the number corresponding to the BT connexion (used to send messages)
+* \param int dest : the ID of the ally (used to send messages)
+*/
 void put_small_fin(uint8_t *sn, uint8_t sn3, uint8_t sn_sonar, int s, int dest);
+
+/**
+* \brief : function used to make the robot run the big arena as the beginner
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn4 : the sequence number of the tacho motor used by the catapult  
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor 
+* \param uint8_t sn_colot : the sequence number of the color sensor
+* \param int* s : the adress where is stored the number corresponding to the BT connexion  
+* \param int dest : the ID of the ally 
+*/
 void big_arena_beginner(uint8_t *sn, uint8_t sn3,uint8_t sn4, uint8_t sn_sonar, int side, int s, int dest);
+
+/**
+* \brief : function used to make the robot run the big arena as the finisher
+* \param uint8_t* sn : the list of the sequence number of the tacho motor used to move 
+* \param uint8_t sn3 : the sequence number of the tacho motor used to open/close the doors 
+* \param uint8_t sn4 : the sequence number of the tacho motor used by the catapult  
+* \param uint8_t sn_sonar : the sequence number of the sonar sensor 
+* \param uint8_t sn_colot : the sequence number of the color sensor
+* \param int* s : the adress where is stored the number corresponding to the BT connexion  
+* \param int dest : the ID of the ally 
+*/
 void big_arena_finisher(uint8_t *sn, uint8_t sn3, uint8_t sn4, uint8_t sn_sonar, uint8_t sn_color, int side, int s, int dest);
-int get_ball(uint8_t *sn, uint8_t sn3, uint8_t sn_sonar, uint8_t sn_color);
